f929a8ff77aa9d588d911e6bf2afa348094f68d3
takeoff <galukhin@gmail.com>
2018-08-03 20:06:32 +0300

    new init

--- app/src/main/java/com/galukhin/introvert/ui/MainActivity.java
+++ app/src/main/java/com/galukhin/introvert/ui/MainActivity.java
@@ -1,22 +1,74 @@
 package com.galukhin.introvert.ui;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.Button;
 
 import com.galukhin.introvert.R;
-import com.galukhin.introvert.model.luna.Fields;
-import com.galukhin.introvert.model.luna.Template;
+import com.galukhin.introvert.model.luna.DbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "LUNA:" + getClass().getSimpleName();
+
+    public static DbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout main = findViewById(R.id.main_layout);
+        dbHelper = new DbHelper(this);
+        final SQLiteDatabase database = MainActivity.dbHelper.getWritableDatabase();
 
-        Template template = new Template(Fields.createButtons(4, this), main);
+
+        Button addNoteBt = findViewById(R.id.add_note);
+        addNoteBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        NoteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button dumpNotesBt = findViewById(R.id.dump_notes);
+        dumpNotesBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DbHelper.dumpNotesTable(database);
+            }
+        });
+
+
+        Button deleteNotesBt = findViewById(R.id.delete_notes_table);
+        deleteNotesBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.deleteNotesTable(MainActivity.dbHelper.getWritableDatabase());
+            }
+        });
+
+        Button createNotesBt = findViewById(R.id.create_notes_table);
+        createNotesBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.createNotesTable(MainActivity.dbHelper.getWritableDatabase());
+            }
+        });
+
+
+        Button dumpLastNoteBt = findViewById(R.id.delete_notes_table);
+        dumpLastNoteBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.dumpNoteTable(MainActivity.dbHelper.getWritableDatabase(), 0);
+            }
+        });
+
     }
 }
